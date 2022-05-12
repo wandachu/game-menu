169d5893b4cf6b1cd07ade02da6a2289420219c1
Wanda Wang <chuwang@wustl.edu>
2022-05-12 15:11:55 -0700

    Remove unneeded import

--- src/main/java/menu/GameMenu.java
+++ src/main/java/menu/GameMenu.java
@@ -14,7 +14,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.io.File;
